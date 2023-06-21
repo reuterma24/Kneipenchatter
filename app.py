47cfc89af7ff07951022adca3c832e6b27ee954a
reuterma24 <martinreuter24@googlemail.com>
2023-06-21 16:23:36 +0200

    WIP

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ from pprint import pprint
 from time import sleep
 from protocol import KademliaProtocol
 from node import Node
+from chat_protocol import ChatProtocol
 import threading
 import sys
 
 
 class App:
-    def __init__(self, port):
+    def __init__(self, port): #rename to Node???
         self.port = port
-        self.protocol = KademliaProtocol(port)
-        threading.Thread(target=self.protocol.listen).start()
-        self.protocol.join()
+        #self.protocol = KademliaProtocol(port)
+        self.chat_protocol = ChatProtocol(port.port)
+        #threading.Thread(target=self.protocol.listen).start()
+        threading.Thread(target=self.chat_protocol.listen).start()
+        #self.protocol.join()
 
     def find_closest_nodes(self, num_nodes):
         nodes = []
@@ -23,7 +26,7 @@ class App:
         return nodes
 
     def test_closest_nodes(self):
-        return [{"u33dc1v0": ("127.0.0.1", 1234)}, {"u33dc1v0": {"127.0.0.1", 1235}}]
+        return [('127.0.0.1', 1234)]
 
     # requested_peers = []
     # session = ["session1":[u3234":1,"u33123":1], "session2":[u3234":1,"u33123":1]
@@ -36,3 +39,5 @@ if __name__ == "__main__":
     port = int(sys.argv[1])
     node = Node(ip="localhost", port=port, ksize=20, alpha=3)
     app = App(node)
+
+    app.chat_protocol.session_creation(42, "Test-Session :)", app.test_closest_nodes())
--- chat_protocol.py
+++ chat_protocol.py
@@ -1,8 +1,63 @@
 import json
+import socket
+from datetime import datetime, timezone
+
+
+class Session:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.peers = []
+        self.msg_buf = []
 
 
 class ChatProtocol:
-    pass
+    sessions = []
+
+    def __init__(self, port_number):
+        self.transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Starting TCP Socket on port {}.".format(port_number))
+        self.transport.bind(("localhost", port_number))
+        # self.transport.setblocking(False)
+
+    def handle(self, data, addr):
+        message = RPC.parse(data)
+        if RPC.is_valid(message):
+            if message["type"] == "session_creation":
+                self.handle_session_creation(message["session_id"], message["session_name"])
+            else:
+                print("Unknown message type: {}".format(message["type"]))
+
+    def listen(self):
+        self.transport.listen()
+        conn, _ = self.transport.accept()
+        print("Vor While Loop")
+        while True:
+            print("Im While Loop")
+            try:
+                data, addr = conn.recvfrom(1024)
+                if data:
+                    self.handle(data, addr)
+            except BlockingIOError:
+                pass
+
+    def handle_session_creation(self, session_id, session_name):
+        print("session recieved: " + str(session_id) + "on port:" + str(self.transport.getsockname()[1]))
+        self.sessions.append(Session(session_id, session_name))
+
+    def session_creation(self, session_id, session_name, possible_peers):
+        for ip, port in possible_peers:
+            self.transport.connect((ip, port))
+            self.transport.send(RPC.serialize(RPC.session_creation(session_id, session_name)))
+
+    def send_msg(self, session_id, msg, sender):
+        session_timestamp = datetime(2023, 1, 1, 12, 42, 59,
+                                     tzinfo=timezone.utc)  # should be the timestamp set during chatroom creation
+        local_time = datetime.now(timezone.utc)
+        diff = local_time - session_timestamp
+        offset = diff.total_seconds()  # use this value to sort incoming messages
+
+        # TODO: iterate over existing TCP addrinfo and send message to each
 
 
 class RPC:
@@ -36,11 +91,13 @@ class RPC:
         }
 
     @staticmethod
-    def msg_to_session(session_id, msg):
+    def msg_to_session(session_id, msg, time_offset, sender):
         return {
             "type": "msg_to_session",
             "session_id": session_id,
             "msg": msg,
+            "time_offset": time_offset,
+            "sender": sender,
         }
 
     @staticmethod
@@ -64,3 +121,12 @@ class RPC:
     @staticmethod
     def parse(data):
         return json.loads(data.decode("utf-8"))
+
+    @staticmethod
+    def is_valid(message):
+        return "type" in message and message["type"] in [
+            "session_creation",
+            "accept_session_creation",
+            "reject_session_creation",
+            # TODO: rest nachtragen
+        ]
